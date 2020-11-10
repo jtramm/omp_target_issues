5ced51bc071bddf7266cadbc5af8e8118312f197
John Tramm <john.tramm@gmail.com>
2020-11-10 19:08:20 +0000

    added simpler version of deep mapper.

--- deep_mapper_simple/main.cpp
+++ deep_mapper_simple/main.cpp
@@ -6,58 +6,44 @@ typedef struct{
   int * arr;
 } MyObject;
 
-#pragma omp declare target
-MyObject * object_arr;
-#pragma omp end declare target
-
-void move_data_to_device(int N)
-{
-  #pragma omp target enter data map(to: object_arr[:N])
-  for( int i = 0; i < N; i++ )
-  {
-    #pragma omp target enter data map(to: object_arr[i].arr[:N])
-  }
-}
-
-void move_data_to_host(int N)
-{
-  #pragma omp target exit data map(from: object_arr[:N])
-  for( int i = 0; i < N; i++ )
-  {
-    #pragma omp target exit data map(from: object_arr[i].arr[:N])
-  }
-}
-
 int main(void)
 {
   int N = 5;
 
-  // Allocate and initialize host array
-  object_arr = (MyObject *) malloc(N * sizeof(MyObject));
+  // Allocate and initialize an array of MyObjects
+  MyObject * objects = (MyObject *) malloc(N * sizeof(MyObject));
   for( int i = 0; i < N; i++ )
   {
-    object_arr[i].val = i;
-    object_arr[i].arr = (int *) malloc(N * sizeof(int));
+    objects[i].val = i;
+    objects[i].arr = (int *) malloc(N * sizeof(int));
     for( int j = 0; j < N; j++ )
     {
-      object_arr[i].arr[j] = i;
+      objects[i].arr[j] = i;
     }
   }
 
-  move_data_to_device(N);
+  #pragma omp target enter data map(to: objects[:N])
+  for( int i = 0; i < N; i++ )
+  {
+    #pragma omp target enter data map(to: objects[i].arr[:N])
+  }
 
   // Execute device kernel
   #pragma omp target teams distribute parallel for
   for( int i = 0; i < N; i++)
   {
-    object_arr[i].val *= 2;
+    objects[i].val *= 2;
     for( int j = 0; j < N; j++ )
     {
-      object_arr[i].arr[j] *= 2;
+      objects[i].arr[j] *= 2;
     }
   }
 
-  move_data_to_host(N);
+  #pragma omp target exit data map(from: objects[:N])
+  for( int i = 0; i < N; i++ )
+  {
+    #pragma omp target exit data map(from: objects[i].arr[:N])
+  }
 
   // Print expected and actual results and check for any errors
   
@@ -65,7 +51,7 @@ int main(void)
   printf("\tExpected Result: 0 2 4 6 8\n");
   printf("\tActual   Result: ");
   for( int i = 0; i < N; i++)
-    printf("%d ", object_arr[i].val);
+    printf("%d ", objects[i].val);
   printf("\n");
 
   printf("Array Values Check:\n");
@@ -85,18 +71,18 @@ int main(void)
     printf("\t");
     for( int j = 0; j < N; j++)
     {
-      printf("%d ", object_arr[i].arr[j]);
+      printf("%d ", objects[i].arr[j]);
     }
     printf("\n");
   }
   
-  if( object_arr[4].val != 8 )
+  if( objects[4].val != 8 )
   {
     printf("Error!\n");
     return 1;
   }
   
-  if( object_arr[4].arr[4] != 8 )
+  if( objects[4].arr[4] != 8 )
   {
     printf("Error!\n");
     return 2;
