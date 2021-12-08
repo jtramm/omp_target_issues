2ee8e11342a81f5331cf88a3cb1354d977015b13
John Tramm <john.tramm@gmail.com>
2021-12-08 16:13:52 +0000

    added balloon3

---
+++ balloon3/main.cpp
@@ -0,0 +1,58 @@
+#include <assert.h>
+#include <omp.h>
+#include <stdio.h>
+#include <algorithm>
+
+double run_empty(){
+  // Time empty kernel
+  double start = omp_get_wtime();
+  #pragma omp target
+  {}
+  double stop = omp_get_wtime();
+  return (stop - start)*1000;
+}
+
+int main(int argc, char *argv[]) {
+  size_t len = 20000;
+
+  // Allocate 2D matrix of size len x len
+  double **arr = (double  **)malloc(len * sizeof(double*));
+  for (int i = 0; i < len; i++) {
+    arr[i] = (double *) calloc(len, sizeof(double));
+  }
+
+  // Avoid JIT time
+  run_empty();
+
+  printf("Empty Kernel Runtimes Before Data Mapping:\n");
+  for (int i = 0; i < 5; i++) {
+    double ms0 = run_empty();
+    printf("  %.5lf ms\n", ms0);
+  }
+
+  // Map 2D matrix to device
+  #pragma omp target enter data map(to : arr[:len])
+  for (int i = 0; i < len; i++) {
+    #pragma omp target enter data map(to : arr[i][:len])
+  }
+
+  printf("Empty Kernel Runtimes After Data Mapping:\n");
+  for (int i = 0; i < 5; i++) {
+    double ms1 = run_empty();
+    printf("  %.5lf ms\n", ms1);
+  }
+
+  // Unmap 2D matrix
+  for (int i = 0; i < len; i++) {
+    #pragma omp target exit data map(delete : arr[i][:len])
+  }
+  #pragma omp target exit data map(delete : arr[:len])
+
+  printf("Empty Kernel Runtimes After Unmapping:\n");
+  for (int i = 0; i < 5; i++) {
+    double ms2 = run_empty();
+    printf("  %.5lf ms\n", ms2);
+  }
+
+  return 0;
+}
